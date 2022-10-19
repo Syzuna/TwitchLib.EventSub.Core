4566c97b36190258ef2c0aa9de2313f2ac58026e
Tobias "Syzuna" Teske <syzuna@syzuna-programs.de>
2022-10-19 22:03:46 +0200

    Add project files.

---
+++ TwitchLib.EventSub.Core/SubscriptionTypes/Channel/ChannelPollProgress.cs
@@ -0,0 +1,17 @@
+﻿using TwitchLib.EventSub.Core.Models.Polls;
+
+namespace TwitchLib.EventSub.Core.SubscriptionTypes.Channel
+{
+    /// <summary>
+    /// Channel Poll Progress subscription type model
+    /// <para>Description:</para>
+    /// <para>Users respond to a poll on a specified channel.</para>
+    /// </summary>
+    public class ChannelPollProgress : ChannelPollBase
+    {
+        /// <summary>
+        /// The time the poll will end.
+        /// </summary>
+        public DateTimeOffset EndsAt { get; set; } = DateTimeOffset.MinValue;
+    }
+}
