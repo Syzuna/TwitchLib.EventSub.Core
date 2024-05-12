8ce71fe51df3f10dfb708a126777da1b6b98c30f
Cole <swiftyspiffy@gmail.com>
2024-05-12 01:30:15 -0700

    feat: ChannelSuspiciousUserMessage, ChannelSuspiciousUserUpdate

---
+++ TwitchLib.EventSub.Core/SubscriptionTypes/Channel/ChannelSuspiciousUserUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TwitchLib.EventSub.Core.Models.ChannelSuspiciousUser;
+
+namespace TwitchLib.EventSub.Core.SubscriptionTypes.Channel
+{
+    public sealed class ChannelSuspiciousUserUpdate : ChannelSuspiciousUserBase
+    {
+        /// <summary>
+        /// The ID of the moderator that updated the treatment for a suspicious user.
+        /// </summary>
+        public string ModeratorUserId { get; set; } = string.Empty;
+        /// <summary>
+        /// The display name of the moderator that updated the treatment for a suspicious user.
+        /// </summary>
+        public string ModeratorUserName { get; set; } = string.Empty;
+        /// <summary>
+        /// The login of the moderator that updated the treatment for a suspicious user.
+        /// </summary>
+        public string ModeratorUserLogin { get; set; } = string.Empty;
+    }
+}
